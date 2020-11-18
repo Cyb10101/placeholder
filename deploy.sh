17f9d04a64b600e005e729af2374b75a191af547
Cyb10101 <cyb10101@gmail.com>
2020-11-18 15:13:56 +0100

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+loadEnvironmentVariables() {
+    if [ -f ".env" ]; then
+      source .env
+    fi
+    if [ -f ".env.local" ]; then
+      source .env.local
+    fi
+}
+
 checkGitMaster() {
     if [[ $(git symbolic-ref --short -q HEAD) != 'master' ]]; then
         echo 'ERROR: Git is not on branch master!'
@@ -21,42 +30,30 @@ checkGit() {
     fi
 }
 
-askDeploy() {
-    read -p 'Deploy? [y/N] ' -n 1 -r
+symfonyUpdateDatabase() {
+    read -p 'Update database schema? [y/N] ' -n 1 -r
     echo
-    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-        [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        ${BIN_PHP} ./bin/console doctrine:schema:update --force
     fi
 }
 
-loadEnvironmentVariables() {
-    if [ -f ".env" ]; then
-      source .env
-    fi
-    if [ -f ".env.local" ]; then
-      source .env.local
-    fi
+symfonyClearCache() {
+    ${BIN_PHP} ./bin/console cache:clear --no-warmup
+    ${BIN_PHP} ./bin/console cache:warmup
 }
-
-checkGitMaster
-checkGit
-askDeploy
 
 loadEnvironmentVariables
 BIN_PHP="${BIN_PHP:-php}"
 BIN_COMPOSER="${BIN_COMPOSER:-composer}"
 
-# Git & Composer
+checkGitMaster
+checkGit
+
 git pull origin master
 ${BIN_PHP} ${BIN_COMPOSER} install
 
-# Update database schema
-read -p 'Update database schema? [y/N] ' -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    ${BIN_PHP} ./bin/console doctrine:schema:update --force
-fi
+symfonyUpdateDatabase
+symfonyClearCache
 
-# Clear cache
-${BIN_PHP} ./bin/console cache:clear --no-warmup
-${BIN_PHP} ./bin/console cache:warmup
+
